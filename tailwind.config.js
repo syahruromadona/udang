b0bce8989a8dc72dbf9fcfab773a7aa079eb274c
syahruromadona <105641369+syahruromadona@users.noreply.github.com>
2023-02-04 00:03:04 +0800

    first step boh

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {},
+    colors: {
+      "orangeMain" : '#f25e0d',
+      "bggelap" : '#0d1116'
+    }
   },
   plugins: [],
 }
