9abb6d3093a814f363e9621359b5d760f2f5c554
syahruromadona <105641369+syahruromadona@users.noreply.github.com>
2023-02-04 21:47:32 +0800

    try .gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["index.html"],
   theme: {
-    extend: {},
-    colors: {
-      "orangeMain" : '#f25e0d',
-      "bggelap" : '#0d1116'
-    }
+    extend: {
+      colors: {
+        "orangeMain" : '#f25e0d',
+        "bggelap" : '#0d1116',
+        "bgterang" : '#161b22',
+        "colourborder" : '#30363d',
+      }
+    },
+
   },
   plugins: [],
 }
